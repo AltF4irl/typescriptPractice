c1ec9dc46e0b65bdf058a418ba4cce5f834580bb
ghostjaby assil <altf4irl@gmail.com>
2024-06-09 18:33:41 +0300

    refractoring the repo

--- 9.b/multiplier.ts
+++ 9.b/multiplier.ts
@@ -1,7 +1,7 @@
 interface MultiplyValues {
   value1: number;
   value2: number;
-};
+}
 
 const parseArguments = (args: string[]): MultiplyValues => {
   if (args.length < 4) throw new Error('not enough args');
@@ -10,21 +10,24 @@ const parseArguments = (args: string[]): MultiplyValues => {
   if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
     return {
       value1: Number(args[2]),
-      value2: Number(args[3])
-    }
+      value2: Number(args[3]),
+    };
   } else {
-    throw new Error('Provided values were not numbers!')
+    throw new Error('Provided values were not numbers!');
   }
-}
+};
 
 const multiplicator = (a: number, b: number, printText: string) => {
   console.log(printText, a * b);
 };
 
-
 try {
-  const {value1, value2} = parseArguments(process.argv);
-  multiplicator(value1, value2, `Multiplied ${value1} and ${value2}, the result is:`);
+  const { value1, value2 } = parseArguments(process.argv);
+  multiplicator(
+    value1,
+    value2,
+    `Multiplied ${value1} and ${value2}, the result is:`
+  );
 } catch (err: unknown) {
-  if (err instanceof Error) console.log(err.message)
+  if (err instanceof Error) console.log(err.message);
 }
